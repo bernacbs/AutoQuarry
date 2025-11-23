ae758a0076dcb5e5f4d12a5985fcae1894de3ceb
becbs <bernardo.andrade@winbooks.com.br>
2025-11-22 21:35:36 -0300

    Change bootloader

--- Quarry Monitor/startup.lua
+++ Quarry Monitor/startup.lua
@@ -1,19 +1,25 @@
 --Quarry monitor startup
 shell.run("clear")
 print("Now Resuming Quarry Monitor on channel 1")
-print("Press any key to quit. You have 10 seconds.")
-if fs.exists("QuarryMonitor.lua") then
-  for i=10, 1, -1 do
+print("Press any key to quit. You have 20 seconds.")
+local event
+if fs.exists("quarryMonitor.lua") then
+  for i=20, 1, -1 do
 	term.setCursorPos(1,6)
 	if i < 10 then
 	  term.write(" ")	
 	end
     print(i)
-    os.sleep(1)
+    os.startTimer(1)
+	event = os.pullEvent()
     if event == "key" then break end
   end
-  print("Opening...")
-  shell.run("QuarryMonitor -receiveChannel 1")
+  if event == "timer" then
+	  print("Opening...")
+	  shell.run("quarryMonitor -auto 1 8")
+  else
+	  print("Key was pressed! Stopped!")
+  end
 else
   print("No monitor file found!!")
 end
